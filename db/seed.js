169d2e8d27a1d6305e90f0c3de5a1a068f7686aa
Leone Varando <lvarando@gmail.com>
2024-01-24 23:28:56 -0600

    Filled in data for seed file

--- db/seed.js
+++ db/seed.js
@@ -1,20 +1,54 @@
-const { User } = require('../src/models');
+const { User, Guild } = require('../src/models');
 const { db } = require('../db/connection');
 
-export default async function seed() {
+(async function seed() {
   const users = [
     {
       userId: 109922839488994,
-      userName: 'cycle_'
+      userName: 'cycle_',
     },
     {
       userId: 109922839427283,
-      userName: 'opal0744'
+      userName: 'opal0744',
+    },
+    {
+      userId: 109922839481029,
+      userName: '.gigglet',
+    },
+    {
+      userId: 109922839421928,
+      userName: 'hemmie49',
+    },
+    {
+      userId: 109922839418293,
+      userName: 'fred_wad',
+    },
+  ]
+
+  const guilds = [
+    {
+      guildId: 1029834572733,
+      channelId: null,
+      checkAmount: 1,
+    },
+    {
+      guildId: 10298345782383,
+      channelId: null,
+      checkAmount: null,
     }
   ]
 
+
   await db.sync({ force: true });
-  await User.bulkCreate(users);
+  const userCreate = await User.bulkCreate(users);
+  const guildCreate = await Guild.bulkCreate(guilds);
+
+  guildCreate[0].addUsers(userCreate[0]);
+  guildCreate[1].addUsers(userCreate[1]);
+
+  for (let i = 0; i < userCreate.length; i++) {
+    guildCreate[Math.floor(Math.random() * 2)].addUsers(userCreate[i]);
+  }
 
   console.log("Users injected");
-}
+})();
