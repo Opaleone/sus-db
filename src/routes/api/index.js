d9993e358db3cc186755e104008d3d73c0455868
Leone Varando <lvarando@gmail.com>
2024-01-24 23:33:10 -0600

    Specifed routes for express.router

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user');
+const guildRoutes = require('./guild')
 
 router.use('/user', userRoutes);
+router.use('/guild', guildRoutes)
 
 module.exports = router;
