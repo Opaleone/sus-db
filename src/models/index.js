1d27fd5576a90813f2ffa129867244ab68be1c48
Leone Varando <lvarando@gmail.com>
2024-01-26 17:56:33 -0600

    Added relations

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,9 @@ const Guild = require('../models/Guild');
 const Check = require('../models/Check')
 
 User.hasMany(Check);
-Guild.hasMany(Check);
+Check.belongsTo(User);
+Guild.hasMany(User);
+User.belongsTo(Guild);
 
 module.exports = {
   User,
