ac6aec73728e6bbf80529e4f82070a31adb064b1
Leone Varando <lvarando@gmail.com>
2024-01-27 18:31:42 -0600

    Changed relations of the three models for a m:m relation

--- src/models/index.js
+++ src/models/index.js
@@ -2,10 +2,14 @@ const User = require('../models/User');
 const Guild = require('../models/Guild');
 const Check = require('../models/Check')
 
+User.belongsToMany(Guild, { through: 'user_guild'})
 User.hasMany(Check);
+
+Guild.belongsToMany(User, { through: 'user_guild'});
+Guild.hasMany(Check);
+
 Check.belongsTo(User);
-Guild.hasMany(User);
-User.belongsTo(Guild);
+Check.belongsTo(Guild);
 
 module.exports = {
   User,
